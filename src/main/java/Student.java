2d146d47674facfcaa625e85012959bb49648c51
manasfire1-svg <manas.fire.1@gmail.com>
2026-09-04 21:34:52 +0530

    Add student validation

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,5 @@
 public class Student extends User implements Reportable {
+
     private String course;
 
     public Student(
@@ -35,15 +36,48 @@ public class Student extends User implements Reportable {
     }
 
     @Override
-public void generateSummary() {
+    public void generateSummary() {
 
-    System.out.println();
-    System.out.println("===== STUDENT SUMMARY =====");
-    System.out.println("Student ID : " + id);
-    System.out.println("Name       : " + name);
-    System.out.println("Course     : " + course);
-    System.out.println("===========================");
-}
+        System.out.println();
+        System.out.println("===== STUDENT SUMMARY =====");
+        System.out.println("Student ID : " + id);
+        System.out.println("Name       : " + name);
+        System.out.println("Course     : " + course);
+        System.out.println("===========================");
+    }
+
+    public void validate() throws InvalidStudentException {
+
+        if (id <= 0) {
+
+            throw new InvalidStudentException(
+                    "Student ID must be greater than zero."
+            );
+        }
+
+        if (name == null || name.trim().isEmpty()) {
+
+            throw new InvalidStudentException(
+                    "Student name cannot be empty."
+            );
+        }
+
+        if (email == null ||
+                !email.contains("@")) {
+
+            throw new InvalidStudentException(
+                    "Invalid email address."
+            );
+        }
+
+        if (course == null ||
+                course.trim().isEmpty()) {
+
+            throw new InvalidStudentException(
+                    "Course cannot be empty."
+            );
+        }
+    }
 
     public void displayDetails() {
 
