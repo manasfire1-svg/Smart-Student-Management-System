f5e58e1665a74926cc79ad0b8b6186e5da1d44bd
manasfire1-svg <manas.fire.1@gmail.com>
2026-09-04 21:24:54 +0530

    Create AcademicRecord.java

---
+++ src/main/java/AcademicRecord.java
@@ -0,0 +1,53 @@
+public class AcademicRecord {
+
+    private int studentId;
+    private String subject;
+    private int marks;
+    private String grade;
+
+    public AcademicRecord(int studentId, String subject, int marks) {
+
+        if (marks < 0 || marks > 100) {
+            throw new IllegalArgumentException(
+                    "Marks must be between 0 and 100."
+            );
+        }
+
+        this.studentId = studentId;
+        this.subject = subject;
+        this.marks = marks;
+        this.grade = calculateGrade(marks);
+    }
+
+    private String calculateGrade(int marks) {
+
+        if (marks >= 90)
+            return "A+";
+        else if (marks >= 80)
+            return "A";
+        else if (marks >= 70)
+            return "B";
+        else if (marks >= 60)
+            return "C";
+        else if (marks >= 50)
+            return "D";
+        else
+            return "F";
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+}
