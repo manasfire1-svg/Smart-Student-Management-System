b08cdf977f0d6a32daca5c5f8609a2c87181c4ee
manasfire1-svg <manas.fire.1@gmail.com>
2026-09-05 08:36:06 +0530

    Create User.java

---
+++ src/main/java/model/User.java
@@ -0,0 +1,24 @@
+package model;
+
+public abstract class User {
+
+    protected int id;
+    protected String name;
+    protected String email;
+
+    public User(int id, String name, String email) {
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public abstract void displayRole();
+
+    public void displayBasicInfo() {
+
+        System.out.println("ID    : " + id);
+        System.out.println("Name  : " + name);
+        System.out.println("Email : " + email);
+    }
+}
